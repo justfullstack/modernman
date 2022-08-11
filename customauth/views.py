b28fbf5879586f0b336ad0601d931cc6bfc72ae6
chegecjn <chegecjn@gmail.com>
2022-08-11 23:21:12 +0300

    updated authentication views: added a signup view

---
+++ customauth/views.py
@@ -0,0 +1,122 @@
+from base64 import urlsafe_b64decode, urlsafe_b64encode
+from django.urls import reverse
+from django.utils.encoding import force_bytes, force_str
+from django.contrib.sites.shortcuts import get_current_site
+from .utils import token_generator
+from django.core.mail import EmailMessage
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from django.views import View
+from customauth.forms import UserCreationForm
+import logging
+
+
+from customauth.models import CustomUser
+
+logger = logging.getLogger(__name__)
+
+
+class SignupView(View):
+
+    def get(self, request):
+
+        form = UserCreationForm()
+
+        return render(request, "auth/signup.html", {"form": form})
+
+    def post(self, request):
+
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            # gather data
+            first_name = request.POST.get("first_name").title()
+            last_name = request.POST.get("last_name").title()
+            email = request.POST.get("email")
+            password = request.POST.get("password1")
+            is_subscribed = request.POST.get("is_subscribed")
+
+            # ensure user does not already exist
+            try:
+                CustomUser.objects.get(email=email)
+                messages.error(request, "Email address already registered!")
+
+            except CustomUser.DoesNotExist:
+
+                user = CustomUser.objects.create_user(
+                    first_name=first_name,
+                    last_name=last_name,
+                    email=email,
+                    password=password,
+                    is_subscribed=is_subscribed
+                )
+                
+                user.is_active = False
+                user.save()
+
+                # send welcome email
+                form.send_mail()
+
+                # generate activation data
+                token = token_generator.make_token(user)
+                uidb64 = urlsafe_b64encode(force_bytes(user.pk))
+
+                domain = get_current_site(request).domain
+
+                link = reverse(
+                    'activate',
+                    kwargs={'uidb64': uidb64, 'token': token}
+                )
+
+                activate_url = f"http://{domain}{link}"
+
+                # send activation  email
+                email_subject = "Activate Your Account"
+                email_body = f"Hello {user.first_name.title()},\nPlease user the link below to activate account!\n{activate_url}."
+
+                mail = EmailMessage(
+                    subject=email_subject,
+                    body=email_body,
+                    from_email='noreply@modernman.com',
+                    to=[email, ],
+                )
+
+                mail.send(fail_silently=False)
+
+                logger.info(f'Activation email successfully sent to {email}')
+
+                messages.success(request, "You signed up successfully!")
+
+                messages.info(
+                    request, "Please check your inbox to activate your account.")
+
+                logger.info(f"Account created successfully for {email}...!")
+
+                return redirect("home")
+
+        return render(request, "auth/signup.html", {"form": form})
+
+
+def activationView(request, uidb64, token):
+    # decode id
+    id = force_str(urlsafe_b64decode(uidb64))
+
+    # get user
+    user = CustomUser.objects.get(pk=id)
+
+    if user.is_active:
+        messages.success(request, "Account already activated!")
+        return redirect('login')
+
+    # check link validity
+    if not token_generator.check_token(user, token):
+        messages.error(request, "Token already used!")
+
+    # activate
+    user.is_active = True
+    user.save()
+
+    logger.info(f"Account activated successfully for user {user.email}...!")
+
+    messages.success(request, "Account activated successfully!")
+    return redirect('login')
--- tests/test_views.py
+++ tests/test_views.py
@@ -1,10 +1,13 @@
+from unittest.mock import patch
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib import auth
 from customauth.forms import UserCreationForm
+from customauth.models import CustomUser
 
 
-class TestPage(TestCase):
-    def test_user_signup_page_loads_correctly(self):
+class AuthenticationView(TestCase):
+    def testUserSignupPageLoadsCorrectly(self):
 
         response = self.client.get(reverse("signup"))
 
@@ -12,3 +15,24 @@ class TestPage(TestCase):
         self.assertTemplateUsed(response, "auth/signup.html")
         self.assertContains(response, "Join ModernMan")
         self.assertIsInstance(response.context["form"], UserCreationForm)
+
+    def testUserSignupPageSubmissionWorks(self):
+        post_data = {
+            "first_name": "First",
+            "last_name": "Last",
+            "email": "user1@gmail.com",
+            "password1": "my_pAssword!",
+            "password2": "my_pAssword!",
+        }
+
+        with patch.object(UserCreationForm, "send_mail") as mock_send:
+            response = self.client.post(
+                reverse("signup"),
+                post_data
+            )
+
+        # confirm redirect
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(CustomUser.objects.filter(
+            email=post_data["email"]).exists())
+        mock_send.assert_called_once()
