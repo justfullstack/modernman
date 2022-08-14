a21c4541cb7ab3422492f9cb384f12b0fdffdf2b
chegecjn <chegecjn@gmail.com>
2022-08-14 04:09:24 +0300

    updated Product model to include tags with default 'all'

--- shop/models.py
+++ shop/models.py
@@ -15,31 +15,6 @@ class ActiveManager(models.Manager):
         return self.filter(active=True)
 
 
-class Product(models.Model):
-
-    objects = ActiveManager()
-
-    name = models.CharField(max_length=32)
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=8, decimal_places=2, blank=False)
-    count = models.IntegerField(blank=False, default=1)
-    slug = models.SlugField(max_length=48, unique=True, blank=False)
-    active = models.BooleanField(default=True)
-    on_sale = models.BooleanField(default=False)
-    date_uploaded = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.name.title()
-
-    def natural_key(self):
-        return self.pk
-
-    @property
-    def is_in_stock(self):
-        '''returns true if product in stock'''
-        return self.count > 0
-
-
 class ProductTagManager(models.Manager):
     '''allows working with natural keys to loaddata'''
 
@@ -64,3 +39,29 @@ class ProductTag(models.Model):
     def natural_key(self):
         '''slug used as natural key since it is unlikely to change'''
         return self.slug
+
+
+class Product(models.Model):
+
+    objects = ActiveManager()
+
+    name = models.CharField(max_length=32)
+    tags = models.ManyToManyField(ProductTag, default="all")
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2, blank=False)
+    count = models.IntegerField(blank=False, default=1)
+    slug = models.SlugField(max_length=48, unique=True, blank=False)
+    active = models.BooleanField(default=True)
+    on_sale = models.BooleanField(default=False)
+    date_uploaded = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name.title()
+
+    def natural_key(self):
+        return self.pk
+
+    @property
+    def is_in_stock(self):
+        '''returns true if product in stock'''
+        return self.count > 0
