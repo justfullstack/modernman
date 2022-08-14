3932c31e03a96c06e2bf308422407cf553e88519
chegecjn <chegecjn@gmail.com>
2022-08-14 03:04:35 +0300

    updated shop models: added a product tags models

--- shop/models.py
+++ shop/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.urls import reverse
 import logging
 
+from matplotlib.image import thumbnail
+
 
 logger = logging.getLogger(__name__)
 
@@ -36,3 +38,29 @@ class Product(models.Model):
     def is_in_stock(self):
         '''returns true if product in stock'''
         return self.count > 0
+
+
+class ProductTagManager(models.Manager):
+    '''allows working with natural keys to loaddata'''
+
+    def get_by_natural_key(self, slug):
+        return self.get(slug=slug)
+
+# MODELS
+
+
+class ProductTag(models.Model):
+    name = models.CharField(max_length=32)
+    slug = models.SlugField(max_length=48)
+    description = models.TextField(blank=True)
+    thumbnail = models.ImageField('tag_thumbnail')
+
+    objects = ProductTagManager()
+
+    def __str__(self):
+        '''returns a string name of each tag'''
+        return self.name.title()
+
+    def natural_key(self):
+        '''slug used as natural key since it is unlikely to change'''
+        return self.slug
