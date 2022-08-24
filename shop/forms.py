96ec2faf4dacbc961761fe8fca5ce912a6e4c9c1
chegecjn <chegecjn@gmail.com>
2022-08-24 22:59:40 +0300

    added  address CRUD views

--- shop/forms.py
+++ shop/forms.py
@@ -4,9 +4,11 @@
 # that done  through the add_to_cart view
 
 
+from django import forms
 from django.forms import inlineformset_factory
+from django.shortcuts import redirect
 from shop.models import Cart, CartLine
-
+from accounts.models import Address
 
 CartLineFormSet = inlineformset_factory(
     Cart,
@@ -15,3 +17,23 @@ CartLineFormSet = inlineformset_factory(
     extra=0,
     #widgets={"quantity": widgets.PlusMinusWidget()},
 )
+
+
+class AddressSelectionForm(forms.Form):
+    '''
+    form dynamically specifies the parameters in the declared fields. In this case we are restricting addresses to the ones that are connected to the current user.
+    '''
+
+    billing_address = forms.ModelChoiceField(queryset=None)
+    shipping_address = forms.ModelChoiceField(queryset=None)
+
+    def __init__(self, user, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        try:
+            queryset = Address.objects.filter(user=user)
+            self.fields['billing_address'].queryset = queryset
+            self.fields['shipping_address'].queryset = queryset
+
+        except Address.DoesNotExist:
+            redirect('create-address')
--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from django.views import View
-from django.views.generic import ListView, DetailView
-from shop.forms import CartLineFormSet
+from django.views.generic import ListView, DetailView, FormView, CreateView, UpdateView, DeleteView
+from accounts.models import Address
+from shop.forms import AddressSelectionForm, CartLineFormSet
 from shop.models import Cart, CartLine, Product, ProductTag
 from django.contrib import messages
+from django.urls import reverse, reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class ProductListView(ListView):
@@ -73,7 +76,8 @@ def addToCart(request):
         messages.error(
             request, f"'{product.name.title().strip('.')}' already in the cart!")
     else:
-        cart.count += 1
+        cart.count += cart.cartline_set.all().count()
+        cart.save()
         cartline.save()
 
         messages.success(
@@ -103,3 +107,56 @@ def manageCart(request):
         return render(request, "shop/cart.html", {"formset": None})
 
     return render(request, "shop/cart.html", {"formset": formset})
+
+
+def completeCheckout(request):
+    return render(request, 'shop/complete_order.html')
+
+
+#########################  SHIPPING: ADDRESSES CRUD VIEWS  ###########################
+class AddressSelectionView(LoginRequiredMixin, FormView):
+
+    template_name = 'shop/select_address.html'
+    form_class = AddressSelectionForm
+    success_url = reverse_lazy('checkout-done')
+
+    def get_form_kwargs(self):
+        '''extracts the user from the request and returns it in a dictionary - dictionary is then passed to the form by the superclass'''
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+    def form_valid(self, form):
+        # delete the cart from the session
+        del self.request.session['cart_id']
+
+        # create order with the submitted addresses data
+        cart = self.request.cart
+        cart.createOrder(
+            form.cleaned_data['billing_address'],
+            form.cleaned_data['shipping_address']
+        )
+
+        return super().form_valid(form)
+
+
+class AddressListView(LoginRequiredMixin, ListView):
+    model = Address
+
+    def get_queryset(self):
+        return self.model.objects.get(user=self.request.user)
+
+
+class AddressCreateView(LoginRequiredMixin, CreateView):
+    model = Address
+    fields = ['address', 'postal_code', ' town',
+              'county', 'city', 'country', 'phone_no']
+
+
+class AddressUpdateView(LoginRequiredMixin, UpdateView):
+    model = Address
+    fields = '__all__'
+
+
+class AddressDeleteView(LoginRequiredMixin, DeleteView):
+    model = Address
