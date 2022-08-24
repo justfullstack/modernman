210a950deb0c7e7b7a12a9b0f774af0aa6f15854
chegecjn <chegecjn@gmail.com>
2022-08-25 01:08:20 +0300

    added address  custom admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from accounts.models import Address
+
 # Register your models here.
+
+
+class AddressAdmin(admin.ModelAdmin):
+    model = Address
+
+    list_display = (
+        'user',
+        'name',
+        'address',
+        'county',
+        'city',
+        'country',
+        'phone_no',
+    )
+
+    readonly_fields = ('user', )
