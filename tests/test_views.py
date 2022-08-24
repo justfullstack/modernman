1ad6f401e661e5101e8ae447159610813d7eb51c
chegecjn <chegecjn@gmail.com>
2022-08-25 00:11:36 +0300

    added tests for address CRUD views

--- tests/test_views.py
+++ tests/test_views.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib import auth
+from accounts.models import Address
 from customauth.forms import UserCreationForm, AuthenticationForm
 from customauth.models import CustomUser
 from shop.models import Cart, CartLine, Product
@@ -135,3 +136,82 @@ class TestShopView(TestCase):
         )
 
         self.assertEquals(CartLine.objects.filter(cart__user=user).count(), 2)
+
+
+class TestAddressPage(TestCase):
+    def testAddressListPageReturnsOnlyOwnedAddresses(self):
+        user1 = CustomUser.objects.create_user(
+            first_name="First",
+            last_name="Last",
+            email="user@gmail.com",
+            password="my_pAssword!"
+        )
+
+        user2 = CustomUser.objects.create_user(
+            first_name="User",
+            last_name="Two",
+            email="usertwo@gmail.com",
+            password="my_pAssword!"
+        )
+
+        Address.objects.create(
+            user=user1,
+            title='MR.',
+            name="First Last",
+            address="127 Kilimani",
+            town='Nairobi',
+            city="Nairobi",
+            county='047',
+            country="KE",
+
+        )
+
+        Address.objects.create(
+            user=user2,
+            title='MR.',
+            name="User Two",
+            address="127 Kilimani",
+            town='Nairobi',
+            city="Nairobi",
+            county='047',
+            country="KE",
+
+        )
+
+        self.client.force_login(user2)
+
+        response = self.client.get(reverse("address_list"))
+
+        self.assertEqual(response.status_code, 200)
+
+        address_list = Address.objects.filter(user=user2)
+
+        self.assertEqual(
+            list(response.context["object_list"]),
+            list(address_list),
+        )
+
+    def testCreateAddressStoresUser(self):
+
+        user1 = CustomUser.objects.create_user(
+            first_name="Three",
+            last_name="Four",
+            email="three_four@gmail.com",
+            password="my_pAssword!"
+        )
+
+        post_data = {
+            'title': 'MR.',
+            'name': "First Last",
+            'address': "1 av st",
+            'town': 'Kambu',
+            'city': "Makueni",
+            'county': '017',
+            'country': "KE",
+
+        }
+
+        self.client.force_login(user1)
+
+        self.client.post(reverse("address_create"), post_data)
+        self.assertTrue(Address.objects.filter(user=user1).exists())
